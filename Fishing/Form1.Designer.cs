843cf1a089015633b1b4723c0992863a20d2268f
Apache9 <palomino219@gmail.com>
2013-07-07 19:32:40 +0800

    Project initialize

---
+++ Fishing/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Capture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxTimes = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButtonFancyCarp = new System.Windows.Forms.RadioButton();
+            this.radioButtonRainbowTrout = new System.Windows.Forms.RadioButton();
+            this.textBoxBaitPos = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBoxPlace = new System.Windows.Forms.GroupBox();
+            this.groupBoxPlace.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(274, 272);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(187, 382);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(274, 343);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxTimes
+            // 
+            this.textBoxTimes.Location = new System.Drawing.Point(310, 57);
+            this.textBoxTimes.Name = "textBoxTimes";
+            this.textBoxTimes.Size = new System.Drawing.Size(94, 21);
+            this.textBoxTimes.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(271, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "次数";
+            // 
+            // radioButtonFancyCarp
+            // 
+            this.radioButtonFancyCarp.AutoSize = true;
+            this.radioButtonFancyCarp.Location = new System.Drawing.Point(18, 22);
+            this.radioButtonFancyCarp.Name = "radioButtonFancyCarp";
+            this.radioButtonFancyCarp.Size = new System.Drawing.Size(59, 16);
+            this.radioButtonFancyCarp.TabIndex = 5;
+            this.radioButtonFancyCarp.TabStop = true;
+            this.radioButtonFancyCarp.Text = "钓锦鲤";
+            this.radioButtonFancyCarp.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonRainbowTrout
+            // 
+            this.radioButtonRainbowTrout.AutoSize = true;
+            this.radioButtonRainbowTrout.Location = new System.Drawing.Point(18, 44);
+            this.radioButtonRainbowTrout.Name = "radioButtonRainbowTrout";
+            this.radioButtonRainbowTrout.Size = new System.Drawing.Size(71, 16);
+            this.radioButtonRainbowTrout.TabIndex = 6;
+            this.radioButtonRainbowTrout.TabStop = true;
+            this.radioButtonRainbowTrout.Text = "钓虹鳟鱼";
+            this.radioButtonRainbowTrout.UseVisualStyleBackColor = true;
+            // 
+            // textBoxBaitPos
+            // 
+            this.textBoxBaitPos.Location = new System.Drawing.Point(310, 90);
+            this.textBoxBaitPos.Name = "textBoxBaitPos";
+            this.textBoxBaitPos.Size = new System.Drawing.Size(94, 21);
+            this.textBoxBaitPos.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "鱼饵位置";
+            // 
+            // groupBoxPlace
+            // 
+            this.groupBoxPlace.Controls.Add(this.radioButtonRainbowTrout);
+            this.groupBoxPlace.Controls.Add(this.radioButtonFancyCarp);
+            this.groupBoxPlace.Location = new System.Drawing.Point(293, 156);
+            this.groupBoxPlace.Name = "groupBoxPlace";
+            this.groupBoxPlace.Size = new System.Drawing.Size(111, 66);
+            this.groupBoxPlace.TabIndex = 9;
+            this.groupBoxPlace.TabStop = false;
+            this.groupBoxPlace.Text = "钓鱼地点";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 421);
+            this.Controls.Add(this.groupBoxPlace);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxBaitPos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxTimes);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Fishing";
+            this.groupBoxPlace.ResumeLayout(false);
+            this.groupBoxPlace.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxTimes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButtonFancyCarp;
+        private System.Windows.Forms.RadioButton radioButtonRainbowTrout;
+        private System.Windows.Forms.TextBox textBoxBaitPos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBoxPlace;
+    }
+}
+
---
+++ Fishing/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+using System.Diagnostics;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+using System.Runtime.InteropServices;
+
+namespace Capture
+{
+
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern UInt32 SendInput(UInt32 nInputs, INPUT[] pInputs, int cbSize);
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct INPUT
+        {
+            [FieldOffset(0)]
+            public Int32 type;
+            [FieldOffset(4)]
+            public KEYBDINPUT ki;
+            [FieldOffset(4)]
+            public MOUSEINPUT mi;
+            [FieldOffset(4)]
+            public HARDWAREINPUT hi;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MOUSEINPUT
+        {
+            public Int32 dx;
+            public Int32 dy;
+            public Int32 mouseData;
+            public Int32 dwFlags;
+            public Int32 time;
+            public IntPtr dwExtraInfo;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct KEYBDINPUT
+        {
+            public Int16 wVk;
+            public Int16 wScan;
+            public Int32 dwFlags;
+            public Int32 time;
+            public IntPtr dwExtraInfo;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct HARDWAREINPUT
+        {
+            public Int32 uMsg;
+            public Int16 wParamL;
+            public Int16 wParamH;
+        }
+
+        private const int INPUT_KEYBOARD = 1;
+        private const int KEYEVENTF_KEYUP = 0x0002;
+        private const int KEYEVENTF_SCANCODE = 0x0008;
+
+        private static INPUT initKeyDown(Microsoft.DirectX.DirectInput.Key key)
+        {
+            INPUT keyDown = new INPUT();
+            keyDown.type = INPUT_KEYBOARD;
+            keyDown.ki.wScan = (Int16)key;
+            keyDown.ki.dwFlags = KEYEVENTF_SCANCODE;
+            return keyDown;
+        }
+
+        private static INPUT initKeyUp(Microsoft.DirectX.DirectInput.Key key)
+        {
+            INPUT keyUp = new INPUT();
+            keyUp.type = INPUT_KEYBOARD;
+            keyUp.ki.wScan = (Int16)key;
+            keyUp.ki.dwFlags = KEYEVENTF_SCANCODE | KEYEVENTF_KEYUP;
+            return keyUp;
+        }
+        private static INPUT[] J_DOWN = new INPUT[1] { initKeyDown(Microsoft.DirectX.DirectInput.Key.J) };
+
+        private static INPUT[] J_UP = new INPUT[1] { initKeyUp(Microsoft.DirectX.DirectInput.Key.J) };
+
+        private static INPUT[] W_DOWN = new INPUT[1] { initKeyDown(Microsoft.DirectX.DirectInput.Key.W) };
+
+        private static INPUT[] W_UP = new INPUT[1] { initKeyUp(Microsoft.DirectX.DirectInput.Key.W) };
+
+        private static Point POINT_FANCY_CARP = new Point(325, 275);
+
+        private static Point POINT_RAINBOW_TROUT = new Point(250, 300);
+
+        private static Size RECTANGLE_SIZE = new Size(50, 40);
+
+        public struct FishingParams
+        {
+            public int times;
+
+            public int baitPos;
+
+            public Point point;
+
+            public Size size;
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private IntPtr edaoWnd;
+
+        private volatile bool quit = false;
+
+        private Thread fishingThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!initWindowHandle())
+            {
+                MessageBox.Show(this, "ED_AO not started");
+                return;
+            }
+            if (fishingThread != null)
+            {
+                MessageBox.Show(this, "Already start");
+                return;
+            }
+            int times;
+            try
+            {
+                times = Int32.Parse(textBoxTimes.Text);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(this, exc.ToString());
+                return;
+            }
+            if (times <= 0)
+            {
+                MessageBox.Show(this, "times must be positive");
+                return;
+            }
+            int baitPos;
+            try
+            {
+                baitPos = Int32.Parse(textBoxBaitPos.Text);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(this, exc.ToString());
+                return;
+            }
+            if (baitPos <= 0)
+            {
+                MessageBox.Show(this, "baitPos must be positive");
+                return;
+            }
+            FishingParams param = new FishingParams();
+            param.times = times;
+            param.baitPos = baitPos;
+            param.size = RECTANGLE_SIZE;
+            if (radioButtonFancyCarp.Checked)
+            {
+                param.point = POINT_FANCY_CARP;
+            }
+            else
+            {
+                param.point = POINT_RAINBOW_TROUT;
+            }
+            SetForegroundWindow(edaoWnd);
+            Thread.Sleep(1000);
+            quit = false;
+            fishingThread = new Thread(new ParameterizedThreadStart(this.fishing));
+            fishingThread.Start(param);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (fishingThread != null)
+            {
+                quit = true;
+                fishingThread.Join();
+                fishingThread = null;
+            }
+        }
+
+        public void fishing(Object obj)
+        {
+            richTextBox1.AppendText("Starting!\r\n");
+            richTextBox1.ScrollToCaret();
+            FishingParams param = (FishingParams)obj;
+            PresentParameters presentParams = new PresentParameters();
+            presentParams.Windowed = true;
+            presentParams.SwapEffect = SwapEffect.Discard;
+            presentParams.PresentFlag = PresentFlag.LockableBackBuffer;
+            presentParams.BackBufferWidth = 1600;
+            presentParams.BackBufferHeight = 900;
+            presentParams.MultiSample = MultiSampleType.None;
+            presentParams.DeviceWindowHandle = this.Handle;
+
+            for (int i = 0; i < 4; i++)
+            {
+                pressKeyJ();
+                Thread.Sleep(2000);
+            }
+            pressKeyW();
+            Thread.Sleep(2000);
+            for (int i = 0; i < 2; i++)
+            {
+                pressKeyJ();
+                Thread.Sleep(2000);
+            }
+            for (int i = 0; i < param.baitPos; i++)
+            {
+                pressKeyW();
+                Thread.Sleep(500);
+            }
+
+            for (int i = param.times; i > 0 && !quit; i--)
+            {
+                pressKeyJ();
+                string text = "Missing!\r\n";
+                for (int j = 0; j < 200 && !quit; j++)
+                {
+                    Device device = new Device(0, DeviceType.Hardware, this, CreateFlags.SoftwareVertexProcessing, presentParams);
+                    Surface surface = device.CreateOffscreenPlainSurface(1600, 900, Format.A8R8G8B8, Pool.SystemMemory);
+                    device.GetFrontBufferData(0, surface);   //copy   frontsurface   to   mySurface
+                    GraphicsStream gStr = Microsoft.DirectX.Direct3D.SurfaceLoader.SaveToStream(ImageFileFormat.Bmp, surface);
+                    Bitmap image = (Bitmap)Bitmap.FromStream(gStr);
+                    gStr.Close();
+                    gStr.Dispose();
+                    surface.ReleaseGraphics();
+                    surface.Dispose();
+                    device.Dispose();
+                    for (int x = 0; x < param.size.Width; x++)
+                    {
+                        for (int y = 0; y < param.size.Height; y++)
+                        {
+                            Color color = image.GetPixel(param.point.X + x, param.point.Y + y);
+                            if (color.R > 250 && color.G > 50 && color.B > 40)
+                            {
+                                pressKeyJ();
+                                text = "Hitting!\r\n";
+                                image.Dispose();
+                                goto outer;
+                            }
+                        }
+                    }
+                    image.Dispose();
+                    Thread.Sleep(50);
+                }
+            outer:
+                richTextBox1.AppendText(i + ". " + text);
+                richTextBox1.ScrollToCaret();
+                Thread.Sleep(5000);
+                pressKeyJ();
+                Thread.Sleep(2000);
+                pressKeyJ();
+                Thread.Sleep(500);
+            }
+            richTextBox1.AppendText("Quiting!\r\n");
+            richTextBox1.ScrollToCaret();
+        }
+
+        private void pressKeyJ()
+        {
+            SendInput(1, J_DOWN, Marshal.SizeOf(J_DOWN[0]));
+            Thread.Sleep(100);
+            SendInput(1, J_UP, Marshal.SizeOf(J_UP[0]));
+        }
+
+        private void pressKeyW()
+        {
+            SendInput(1, W_DOWN, Marshal.SizeOf(W_DOWN[0]));
+            Thread.Sleep(100);
+            SendInput(1, W_UP, Marshal.SizeOf(W_UP[0]));
+        }
+
+        private bool initWindowHandle()
+        {
+            Process[] ps = Process.GetProcessesByName("ED_AO");
+            if (ps.Length == 0)
+            {
+                return false;
+            }
+            Process p = ps[0];
+            edaoWnd = p.MainWindowHandle;
+            return true;
+        }
+
+    }
+}
